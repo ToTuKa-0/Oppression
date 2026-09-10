3631d675390485f720ec133925bb08877837d899
Hatakeyama Kakeru <g025g1015@g.neec.ac.jp>
2026-09-10 23:53:40 +0900

    [add]各種バフ・デバフの基本的な処理を実装

---
+++ Assets/Scripts/Buff_&_Debuff/Buff_SpeedUp.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Buff_SpeedUp : MonoBehaviour
+{
+    // èâä˙ë¨ìx(v0)
+    public float initialSpeed = 0.7f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.down * initialSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
